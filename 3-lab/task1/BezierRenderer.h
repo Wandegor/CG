cfaba194174df2ccf84db66d068f52a96ccdfa24
wander122156 <sov48656@mail.ru>
2026-03-09 22:12:55 +0300

    3.1 fix: вынес создание буферов и отрисовку кривой в класс

---
+++ 3-lab/task1/BezierRenderer.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <glad/gl.h>
+#include <vector>
+#include "Bezier.h"
+#include "Common/Point.h"
+
+class BezierRenderer
+{
+private:
+    GLuint curveVAO, curveVBO;
+    GLuint pointsVAO, pointsVBO;
+    GLuint dashVAO, dashVBO;
+    size_t curveCount;
+    size_t dashCount;
+
+    static void CreateBuffer(const std::vector<Point>& points, GLuint& VAO, GLuint& VBO)
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(Point), points.data(), GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Point), (void *)0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+
+    static void CreateBufferFixed(const Point* points, size_t count, GLuint& VAO, GLuint& VBO)
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, count * sizeof(Point), points, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Point), (void *)0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+
+public:
+    BezierRenderer(Bezier& bezier)
+    {
+        auto curvePoints = bezier.GenerateCurvePoints();
+        auto dashPoints = bezier.GenerateDashedLines();
+        curveCount = curvePoints.size();
+        dashCount = dashPoints.size();
+
+        CreateBuffer(curvePoints, curveVAO, curveVBO);
+        CreateBufferFixed(bezier.ControlPoints, 4, pointsVAO, pointsVBO);
+        CreateBuffer(dashPoints, dashVAO, dashVBO);
+    }
+
+    ~BezierRenderer()
+    {
+        glDeleteVertexArrays(1, &curveVAO);
+        glDeleteBuffers(1, &curveVBO);
+        glDeleteVertexArrays(1, &pointsVAO);
+        glDeleteBuffers(1, &pointsVBO);
+        glDeleteVertexArrays(1, &dashVAO);
+        glDeleteBuffers(1, &dashVBO);
+    }
+
+    void DrawDashes(GLint colorLoc) const
+    {
+        glUniform4f(colorLoc, 0.6f, 0.6f, 0.6f, 1.0f);
+        glBindVertexArray(dashVAO);
+        glDrawArrays(GL_LINES, 0, dashCount);
+    }
+
+    void DrawCurve(GLint colorLoc) const
+    {
+        glUniform4f(colorLoc, 0.0f, 0.0f, 1.0f, 1.0f);
+        glBindVertexArray(curveVAO);
+        glDrawArrays(GL_LINE_STRIP, 0, curveCount);
+    }
+
+    void DrawPoints(GLint colorLoc) const
+    {
+        glUniform4f(colorLoc, 0.0f, 1.0f, 0.0f, 1.0f);
+        glBindVertexArray(pointsVAO);
+        glDrawArrays(GL_POINTS, 0, 1);
+        glDrawArrays(GL_POINTS, 1, 1);
+        glDrawArrays(GL_POINTS, 2, 1);
+        glDrawArrays(GL_POINTS, 3, 1);
+    }
+};
--- 3-lab/task1/main.cpp
+++ 3-lab/task1/main.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <fstream>
 #include "Bezier.h"
+#include "BezierRenderer.h"
 #include "Window.h"
 #include "Shaders/Shader.h"
 #include "Common/Point.h"
@@ -12,28 +13,9 @@
 const char* pVSFileName = "shader.vs";
 const char* pFSFileName = "shader.fs";
 
-// Выход по Escape
-void ProcessInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// Изменение размера окна
-void Framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
 int main()
 {
     Bezier bezier;
-    // Инициализация GLFW
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
 
     Window window(1600, 1000, "Bezier");
 
@@ -43,11 +25,6 @@ int main()
         return -1;
     }
 
-    glViewport(0, 0, 1600, 1000);
-
-    auto curvePoints = bezier.GenerateCurvePoints();
-    auto dashPoints = bezier.GenerateDashedLines();
-
     // Шейдеры
     Shader shader(pVSFileName, pFSFileName);
     GLuint shaderProgram = shader.GetProgram();
@@ -55,40 +32,9 @@ int main()
     int colorLocation = glGetUniformLocation(shaderProgram, "uColor");
 
     // Кривая
-    GLuint curveVAO, curveVBO;
-    glGenVertexArrays(1, &curveVAO);
-    glGenBuffers(1, &curveVBO);
-    glBindVertexArray(curveVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, curveVBO);
-    glBufferData(GL_ARRAY_BUFFER, curvePoints.size() * sizeof(Point), curvePoints.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Point), nullptr);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
     // Контрольные точки
-    GLuint pointsVAO, pointsVBO;
-    glGenVertexArrays(1, &pointsVAO);
-    glGenBuffers(1, &pointsVBO);
-    glBindVertexArray(pointsVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(bezier.ControlPoints), bezier.ControlPoints, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Point), nullptr);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
     // Пунктир
-    unsigned int dashVAO, dashVBO;
-    glGenVertexArrays(1, &dashVAO);
-    glGenBuffers(1, &dashVBO);
-    glBindVertexArray(dashVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, dashVBO);
-    glBufferData(GL_ARRAY_BUFFER, dashPoints.size() * sizeof(Point), dashPoints.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Point), (void *)nullptr);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    BezierRenderer renderer(bezier);
 
     // Размер точек
     glPointSize(bezier.GetPointSize());
@@ -97,7 +43,8 @@ int main()
     glEnable(GL_LINE_SMOOTH);
     glEnable(GL_MULTISAMPLE);
 
-    // Главный цикл
+    glViewport(0, 0, window.GetWidth(), window.GetHeight());
+
     while (!window.ShouldClose())
     {
         window.ProcessInput();
@@ -108,40 +55,17 @@ int main()
 
         glUseProgram(shaderProgram);
 
-        // пунктирные линии (серые)
-        glUniform4f(colorLocation, 0.6f, 0.6f, 0.6f, 1.0f);
-        glBindVertexArray(dashVAO);
-        glDrawArrays(GL_LINES, 0, dashPoints.size());
+        renderer.DrawDashes(colorLocation);
+        renderer.DrawCurve(colorLocation);
+        renderer.DrawPoints(colorLocation);
 
-        // кривую Безье (синяя)
-        glUniform4f(colorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-        glBindVertexArray(curveVAO);
-        glDrawArrays(GL_LINE_STRIP, 0, curvePoints.size());
-
-        // контрольные точки
-        glUniform4f(colorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
-        glBindVertexArray(pointsVAO);
-        glDrawArrays(GL_POINTS, 0, 1);
-        glDrawArrays(GL_POINTS, 1, 1);
-        glDrawArrays(GL_POINTS, 2, 1);
-        glDrawArrays(GL_POINTS, 3, 1);
-
-        // Отвязываем VAO
+        // Отвязать VAO
         glBindVertexArray(0);
 
-        // Обмен буферов и обработка событий
         window.SwapBuffers();
+
         window.PollEvents();
     }
-
-    // Очистка ресурсов
-    glDeleteVertexArrays(1, &curveVAO);
-    glDeleteBuffers(1, &curveVBO);
-    glDeleteVertexArrays(1, &pointsVAO);
-    glDeleteBuffers(1, &pointsVBO);
-    glDeleteVertexArrays(1, &dashVAO);
-    glDeleteBuffers(1, &dashVBO);
-    glDeleteProgram(shaderProgram);
 
     return 0;
 }
