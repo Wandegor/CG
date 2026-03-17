3a2d64a653288f93f2b4fc50dec75147f86401a4
wander122156 <sov48656@mal.ru>
2026-03-17 09:56:12 +0300

    3.2 feat: разнес по функциям метод Update

--- 3-lab/task2/SceneRenderer.h
+++ 3-lab/task2/SceneRenderer.h
@@ -11,7 +11,7 @@
 
 struct GameObject
 {
-    Drawable* drawable;
+    Drawable *drawable;
     Mat3 model;
     float color[4];
 };
@@ -21,7 +21,7 @@ class SceneRenderer
 private:
     std::vector<GameObject> m_objects;
 
-    Shader* shader;
+    Shader *shader;
     float m_aspect;
 
     GLint colorLoc;
@@ -59,16 +59,16 @@ private:
     int particleCount = 4;
 
 public:
-    explicit SceneRenderer(Shader* shader, float aspect)
-        : shader(shader), m_aspect(aspect),
-          connectingRod(std::vector<Point>{
-              {0.0f, 0.0f},
-              {0.0f, -0.45f}
-          }),
-          crankshaft(std::vector<Point>{
-              {0.0f, 0.0f},
-              {0, m_crankLen}
-          })
+    explicit SceneRenderer(Shader *shader, float aspect)
+            : shader(shader), m_aspect(aspect),
+              connectingRod(std::vector<Point>{
+                      {0.0f, 0.0f},
+                      {0.0f, -0.45f}
+              }),
+              crankshaft(std::vector<Point>{
+                      {0.0f, 0.0f},
+                      {0,    m_crankLen}
+              })
     {
         GLuint program = shader->GetProgram();
 
@@ -81,101 +81,101 @@ public:
 
         // Блок цилиндра ФОН
         m_objects.push_back({
-            &cylinderBlockBack,
-            Mat3::translation(0.0f, -0.15f),
-            {0.25f, 0.25f, 0.25f, 1}
-        });
+                                    &cylinderBlockBack,
+                                    Mat3::translation(0.0f, -0.15f),
+                                    {0.25f, 0.25f, 0.25f, 1}
+                            });
 
         // Маховик ФОН
         m_objects.push_back({
-            &flywheelBack,
-            Mat3::translation(0.0f, -0.6f),
-            {0.25f, 0.25f, 0.25f, 1}
-        });
+                                    &flywheelBack,
+                                    Mat3::translation(0.0f, -0.6f),
+                                    {0.25f, 0.25f, 0.25f, 1}
+                            });
         // Блок цилиндра
         m_objects.push_back({
-            &cylinderBlock,
-            Mat3::translation(0.0f, -0.15f),
-            {0.55f, 0.55f, 0.55f, 1}
-        });
+                                    &cylinderBlock,
+                                    Mat3::translation(0.0f, -0.15f),
+                                    {0.55f, 0.55f, 0.55f, 1}
+                            });
 
         // Маховик
         m_objects.push_back({
-            &flywheel,
-            Mat3::translation(0.0f, -0.6f),
-            {0.55f, 0.55f, 0.55f, 1}
-        });
+                                    &flywheel,
+                                    Mat3::translation(0.0f, -0.6f),
+                                    {0.55f, 0.55f, 0.55f, 1}
+                            });
 
         // Поршень
         m_objects.push_back({
-            &piston,
-            Mat3::translation(0.0f, 0.0f),
-            {0.8f, 0.2f, 0.2f, 1}
-        });
+                                    &piston,
+                                    Mat3::translation(0.0f, 0.0f),
+                                    {0.8f, 0.2f, 0.2f, 1}
+                            });
 
         // Шатун
         m_objects.push_back({
-            &connectingRod,
-            Mat3::translation(0.0f, 0.0f),
-            {0, 0, 0, 1}
-        });
+                                    &connectingRod,
+                                    Mat3::translation(0.0f, 0.0f),
+                                    {0, 0, 0, 1}
+                            });
 
         // Коленвал
         m_objects.push_back({
-            &crankshaft,
-            Mat3::translation(0.0f, -0.6f),
-            {0, 0, 0, 1}
-        });
+                                    &crankshaft,
+                                    Mat3::translation(0.0f, -0.6f),
+                                    {0, 0, 0, 1}
+                            });
 
         // Свеча
         m_objects.push_back({
-            &sparkPlug,
-            Mat3::translation(0.0f, 0.16f),
-            {0, 0, 0, 1}
-        });
+                                    &sparkPlug,
+                                    Mat3::translation(0.0f, 0.16f),
+                                    {0, 0, 0, 1}
+                            });
 
         // Трубы клапанов
         m_objects.push_back({
-            &valveLeftCylinder,
-            Mat3::translation(-0.1f, 0.17f),
-            {0.8f, 0.6f, 0.8f, 1}
-        });
+                                    &valveLeftCylinder,
+                                    Mat3::translation(-0.1f, 0.17f),
+                                    {0.8f, 0.6f, 0.8f, 1}
+                            });
         m_objects.push_back({
-            &valveRightCylinder,
-            Mat3::translation(0.1f, 0.17f),
-            {0.6f, 0.8f, 0.8f, 1}
-        });
+                                    &valveRightCylinder,
+                                    Mat3::translation(0.1f, 0.17f),
+                                    {0.6f, 0.8f, 0.8f, 1}
+                            });
 
         // клапаны
         m_objects.push_back({
-            &valveLeft,
-            Mat3::translation(-0.1f, 0.12f),
-            {0.2f, 0.2f, 0.5f, 1}
-        });
+                                    &valveLeft,
+                                    Mat3::translation(-0.1f, 0.12f),
+                                    {0.2f, 0.2f, 0.5f, 1}
+                            });
         m_objects.push_back({
-            &valveRight,
-            Mat3::translation(0.1f, 0.12f),
-            {0.2f, 0.2f, 0.5f, 1}
-        });
+                                    &valveRight,
+                                    Mat3::translation(0.1f, 0.12f),
+                                    {0.2f, 0.2f, 0.5f, 1}
+                            });
 
         // вых газ
         exhaustParticles.resize(particleCount, Circle(0.03f));
         for (int i = 0; i < particleCount; ++i)
         {
             m_objects.push_back({
-                &exhaustParticles[i],
-                Mat3::translation(0.1f, 0.15f) * Mat3::scale(0.0f, 0.0f),
-                {0.5f, 0.5f, 0.5f, 1.0f}
-            });
+                                        &exhaustParticles[i],
+                                        Mat3::translation(0.1f, 0.15f) * Mat3::scale(0.0f, 0.0f),
+                                        {0.5f, 0.5f, 0.5f, 1.0f}
+                                });
             exhaustIndices.push_back(m_objects.size() - 1);
         }
 
         // Вспышка на свече
         m_objects.push_back({
-            &sparkFlash,
-            Mat3::translation(0.0f, 0.1f),
-            {1.0f, 1.0f, 0.0f, 1.0f}
-        });
+                                    &sparkFlash,
+                                    Mat3::translation(0.0f, 0.1f),
+                                    {1.0f, 1.0f, 0.0f, 1.0f}
+                            });
 
         flashIndex = m_objects.size() - 1;
     }
@@ -191,7 +191,7 @@ public:
 
         glLineWidth(6.0f);
 
-        for (const auto& obj: m_objects)
+        for (const auto &obj: m_objects)
         {
             glUniformMatrix3fv(modelLoc, 1, GL_TRUE, obj.model.data);
             glUniform4fv(colorLoc, 1, obj.color);
@@ -203,69 +203,80 @@ public:
     void Update(float deltaTime)
     {
         float angularSpeed = 2.0f;
-
         angle += angularSpeed * deltaTime;
 
-        float crankX = crankRadius * -std::sin(angle); // x = R·sin(angle),
-        float crankY = crankCenterY + crankRadius * std::cos(angle); // y = y0 + R·sin(angle).
+        UpdatePistonAndCrank();
+
+        UpdateValves();
+
+        UpdateExhaustParticles();
+        UpdateFlash();
+    }
+
+    void SetAspect(float aspect)
+    { m_aspect = aspect; }
+
+private:
+    void UpdatePistonAndCrank()
+    {
+
+        float crankX = crankRadius * -std::sin(angle); // x = R·sin(angle)
+        float crankY = crankCenterY + crankRadius * std::cos(angle); // y = y0 + R·cos(angle)
         float pistonY = crankY + std::sqrt(rodLen * rodLen - crankX * crankX);
 
         // поршень
         m_objects[4].model = Mat3::translation(0.0f, pistonY);
 
         float rodAngle = std::atan2(crankX, pistonY - crankY);
-
         // шатун
-        m_objects[5].model =
-                Mat3::translation(0.0f, pistonY) *
-                Mat3::rotation(rodAngle);
+        m_objects[5].model = Mat3::translation(0.0f, pistonY) * Mat3::rotation(rodAngle);
 
         // коленвал
-        m_objects[6].model =
-                Mat3::translation(0.0f, crankCenterY) *
-                Mat3::rotation(angle);
+        m_objects[6].model = Mat3::translation(0.0f, crankCenterY) * Mat3::rotation(angle);
+    }
 
+    void UpdateValves()
+    {
         float maxLift = 0.04f;
-
         float revolutions = angle / (2.0f * M_PI);
         int turnNumber = static_cast<int>(std::floor(revolutions));
-
 
         bool leftActive = (turnNumber % 2 == 0);
         bool rightActive = !leftActive;
 
-        float liftLeft;
-        float liftRight;
+        float liftLeft = 0.0f;
+        float liftRight = 0.0f;
 
         if (leftActive)
         {
             liftLeft = std::max(0.0f, std::sin(angle));
-            liftRight = 0.0f;
         }
-
         if (rightActive)
         {
-            liftLeft = 0.0f;
-            liftRight = std::max(0.0f, std::sin(angle + float(M_PI)*1.15f));
+            liftRight = std::max(0.0f, std::sin(angle + float(M_PI) * 1.15f));
         }
 
-        // клапаны
         m_objects[10].model = Mat3::translation(-0.1f, 0.12f - liftLeft * maxLift);
         m_objects[11].model = Mat3::translation(0.1f, 0.12f - liftRight * maxLift);
+    }
 
-        // Выпуск газов
+    void UpdateExhaustParticles()
+    {
         float baseX = 0.1f;
         float baseY = 0.15f;
         float globalPhaseShift = -0.7f;
+        float revolutions = angle / (2.0f * M_PI);
+        int turnNumber = static_cast<int>(std::floor(revolutions));
+        bool rightActive = (turnNumber % 2 != 0);
 
         for (int i = 0; i < particleCount; ++i)
         {
             float intensity = 0.0f;
             if (rightActive)
             {
-                float phaseShift = globalPhaseShift + i * 0.3f; // сдвиг каждого
+                float phaseShift = globalPhaseShift + i * 0.3f;
                 float shiftedLift = std::max(0.0f, std::sin(angle + float(M_PI) + phaseShift));
-                float threshold = 0.93f; // порог
+                float threshold = 0.93f;
                 if (shiftedLift > threshold)
                 {
                     intensity = (shiftedLift - threshold) / (1.0f - threshold);
@@ -279,10 +290,12 @@ public:
                         Mat3::scale(intensity, intensity);
             }
         }
+    }
 
-        // вспышка
+    void UpdateFlash()
+    {
         float flashIntensity = 0.0f;
-
+        float revolutions = angle / (2.0f * M_PI);
         float nearestInt = std::round(revolutions);
         float distance = std::abs(angle - nearestInt * 2.0f * M_PI);
         float epsilon = 0.4f;
@@ -299,6 +312,4 @@ public:
                     Mat3::scale(flashIntensity, flashIntensity);
         }
     }
-
-    void SetAspect(float aspect) { m_aspect = aspect; }
 };
