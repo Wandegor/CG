1f5108963713f3a2e1b14438f7603871677dd1c9
wander122156 <sov48656@mail.ru>
2026-03-19 14:14:44 +0300

    4.1 feat: юзнул код статичного кубика малова

--- 4-lab/task1/Window.h
+++ 4-lab/task1/Window.h
@@ -1,95 +1,20 @@
 #pragma once
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include "BaseWindow.h"
+#include "Cube.h"
 
-class Window
+class Window : public BaseWindow
 {
-private:
-    GLFWwindow* m_window;
-    int m_width, m_height;
-    const char* m_title;
-
-    static void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
-    {
-        glViewport(0, 0, width, height);
-        Window* newWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
-        if (newWindow)
-            {
-            newWindow->m_width = width;
-            newWindow->m_height = height;
-        }
-    }
-
 public:
-    Window(int width, int height, const char* title) : m_width(width), m_height(height), m_title(title)
-    {
-        if (!glfwInit())
-        {
-            std::cerr << "Failed to initialize GLFW" << std::endl;
-            exit(-1);
-        }
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_SAMPLES, 4);
+	Window(int w, int h, const char* title);
 
-        m_window = glfwCreateWindow(width, height, title, nullptr, nullptr);
-        if (!m_window)
-        {
-            std::cerr << "Failed to create GLFW window" << std::endl;
-            glfwTerminate();
-            exit(-1);
-        }
-        glfwMakeContextCurrent(m_window);
-        glfwSetWindowUserPointer(m_window, this); // Сохраняем указатель на объект для использования в колбэках
-        glfwSetFramebufferSizeCallback(m_window, FramebufferSizeCallback);
-    }
+private:
+	void OnResize(int width, int height) override;
 
-    ~Window()
-    {
-        glfwDestroyWindow(m_window);
-        glfwTerminate();
-    }
+	void OnRunStart() override;
 
-    bool ShouldClose() const
-    {
-        return glfwWindowShouldClose(m_window);
-    }
+	void Draw(int width, int height) override;
 
-    void SwapBuffers()
-    {
-        glfwSwapBuffers(m_window);
-    }
+	static void SetupProjectionMatrix(int width, int height);
 
-    void PollEvents()
-    {
-        glfwPollEvents();
-    }
-
-    void ProcessInput()
-    {
-        if (glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(m_window, true);
-    }
-
-    bool IsMouseButtonPressed(int button) const
-    {
-        return glfwGetMouseButton(m_window, button) == GLFW_PRESS;
-    }
-
-    void GetMousePos(double& x, double& y) const
-    {
-        glfwGetCursorPos(m_window, &x, &y);
-    }
-
-    void GetNormalizedMousePos(double& nx, double& ny) const
-    {
-        double x, y;
-        glfwGetCursorPos(m_window, &x, &y);
-        nx = (x / m_width) * 2.0 - 1.0;
-        ny = 1.0 - (y / m_height) * 2.0;
-    }
-
-    [[nodiscard]] int GetWidth() const { return m_width; }
-    [[nodiscard]] int GetHeight() const { return m_height; }
+	Cube m_cube;
 };
--- 4-lab/task1/main.cpp
+++ 4-lab/task1/main.cpp
@@ -1,43 +1,10 @@
-#include <glad/gl.h>
+﻿#include "pch.h"
+#include "GLFWInitializer.h"
 #include "Window.h"
-#include "SceneRenderer.h"
-#include "Shaders/Shader.h"
 
 int main()
 {
-
-    int SCR_WIDTH = 1000;
-    int SCR_HEIGHT = 1000;
-
-    Window window(SCR_WIDTH, SCR_HEIGHT, "Engine");
-
-    if (!gladLoadGL(glfwGetProcAddress))
-        return -1;
-
-    Shader shader("shader2.vs", "shader2.fs");
-
-    SceneRenderer renderer(&shader, static_cast<float>(SCR_HEIGHT)/static_cast<float>(SCR_WIDTH));
-
-    float lastTime = glfwGetTime();
-
-    while (!window.ShouldClose())
-    {
-        float currentTime = glfwGetTime();
-        float deltaTime = currentTime - lastTime;
-        lastTime = currentTime;
-
-        window.ProcessInput();
-
-        float aspect = static_cast<float>(window.GetHeight()) / static_cast<float>(window.GetWidth());
-        renderer.SetAspect(aspect);
-
-        glClearColor(0.9f,0.9f,0.9f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        renderer.Update(deltaTime);
-        renderer.DrawScene();
-
-        window.SwapBuffers();
-        window.PollEvents();
-    }
+	GLFWInitializer initGLFW;
+	Window window{ 800, 600, "3D Cube" };
+	window.Run();
 }
