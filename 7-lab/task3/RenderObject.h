e48bb14575eb18f1b16d0bb304b826412d2f4127
wander122156 <sov48656@mail.ru>
2026-04-27 23:51:24 +0300

    7.3 init: сделал сферу

---
+++ 7-lab/task3/RenderObject.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <vector>
+#include <glad/gl.h>
+
+class RenderObject
+{
+public:
+    RenderObject(const std::vector<float>& vertices, const std::vector<unsigned int>& indices, int vertexSize = 3)
+    {
+        m_indexCount = static_cast<GLsizei>(indices.size());
+
+        glGenVertexArrays(1, &m_vao);
+        glGenBuffers(1, &m_vbo);
+        glGenBuffers(1, &m_ebo);
+
+        glBindVertexArray(m_vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(0);
+    }
+
+    ~RenderObject()
+    {
+        glDeleteVertexArrays(1, &m_vao);
+        glDeleteBuffers(1, &m_vbo);
+    }
+
+    void Draw(GLenum mode = GL_TRIANGLES) const
+    {
+        glBindVertexArray(m_vao);
+        glDrawElements(mode, m_indexCount, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+    }
+
+private:
+    GLuint m_vao = 0, m_vbo = 0, m_ebo = 0;
+    GLsizei m_indexCount;
+};
---
+++ 7-lab/task3/Window.cpp
@@ -0,0 +1,106 @@
+﻿#include "pch.h"
+#include "Window.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "ShapeFactory.h"
+
+namespace
+{
+    // Угол обзора по вертикали
+    constexpr float FIELD_OF_VIEW = 60 * M_PI / 180.0;
+
+    constexpr float Z_NEAR = 0.02f;
+    constexpr float Z_FAR = 50.0f;
+
+    const char* pVSFileName = "shader.vs";
+    const char* pFSFileName = "shader.fs";
+} // namespace
+
+Window::Window(int w, int h, const char* title, Presenter& presenter)
+    : BaseWindow(w, h, title), m_presenter(presenter),
+      m_lastTime(glfwGetTime()) {}
+
+void Window::OnKey(int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(GetWindow(), GLFW_TRUE);
+    }
+
+    m_presenter.OnKey(key, action);
+}
+
+void Window::OnMouseButton(int button, int action, int mods)
+{
+    m_presenter.OnMouseButton(button, action);
+}
+
+void Window::OnMouseMove(double x, double y)
+{
+    m_presenter.OnMouseMove(x, y);
+}
+
+void Window::OnResize(int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void Window::OnRunStart()
+{
+    m_shader = std::make_unique<Shader>(pVSFileName, pFSFileName);
+
+    auto sphereData = ShapeFactory::CreateSphere(20);
+
+    m_sphere = std::make_unique<RenderObject>(sphereData.vertices, sphereData.indices);
+
+    glEnable(GL_DEPTH_TEST);
+}
+
+void Window::SetupLighting() {}
+
+GLuint Window::LoadTexture(const char* path) {}
+
+void Window::Draw(int width, int height)
+{
+    double currentTime = glfwGetTime();
+    float deltaTime = static_cast<float>(currentTime - m_lastTime);
+    m_lastTime = currentTime;
+
+    m_presenter.UpdateMovement(deltaTime);
+
+    glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(m_shader->GetProgram());
+
+    float aspect = static_cast<float>(width) / static_cast<float>(height);
+
+    SetupCameraMatrix(aspect);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    m_sphere->Draw(GL_TRIANGLES);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+void Window::SetupCameraMatrix(float aspect)
+{
+    glm::mat4 projection = glm::perspective(
+        FIELD_OF_VIEW,
+        aspect,
+        Z_NEAR,
+        Z_FAR);
+
+    glm::vec3 pos = m_presenter.GetCameraPos();
+    glm::vec3 front = m_presenter.GetCameraFront();
+    glm::vec3 up = m_presenter.GetCameraUp();
+
+    glm::mat4 view = glm::lookAt(pos, pos + front, up);
+
+    glm::mat4 model = glm::mat4(1.0f);
+
+    glUniformMatrix4fv(glGetUniformLocation(m_shader->GetProgram(), "projection"), 1, GL_FALSE,
+                       glm::value_ptr(projection));
+    glUniformMatrix4fv(glGetUniformLocation(m_shader->GetProgram(), "view"), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(m_shader->GetProgram(), "model"), 1, GL_FALSE, glm::value_ptr(model));
+}
