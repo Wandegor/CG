5c83a8969d4216626cce34fccf3518ba65a06288
wander122156 <sov48656@mail.ru>
2026-05-11 21:40:42 +0300

    8.3 feat: минимальный параболоид(без крышки)

---
+++ 8-lab/task3/Shaders/Shader.h
@@ -0,0 +1,107 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <glad/gl.h>
+#include "Util.h"
+
+class Shader
+{
+    GLuint program;
+
+    static void AddShader(GLuint shaderProgram, const char* pShaderText, GLenum shaderType)
+    {
+        GLuint shader = glCreateShader(shaderType);
+
+        if (shader == 0)
+        {
+            fprintf(stderr, "Error creating shader%d\n", shaderType);
+            exit(0);
+        }
+
+        const GLchar* p[1];
+        p[0] = pShaderText;
+
+        GLint Length[1];
+        Length[0] = std::strlen(pShaderText);
+
+        glShaderSource(shader, 1, p, Length);
+        glCompileShader(shader);
+
+        GLint success;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+        if (!success)
+        {
+            GLchar infoLog[1024];
+            glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+            fprintf(stderr, "Error compiling shader type %d:\n%s\n", shaderType, infoLog);
+            exit(1);
+        }
+
+        glAttachShader(shaderProgram, shader);
+        glDeleteShader(shader);
+    }
+
+    // Проверка линковки программы
+    static void CheckProgramLinking(unsigned int program)
+    {
+        int success;
+        char infoLog[512];
+        glGetProgramiv(program, GL_LINK_STATUS, &success);
+        if (success == 0)
+        {
+            glGetProgramInfoLog(program, 512, nullptr, infoLog);
+            std::cerr << "Error linking shader program\n" << infoLog << std::endl;
+            exit(1);
+        }
+
+        glValidateProgram(program);
+        glGetProgramiv(program, GL_VALIDATE_STATUS, &success);
+        if (success == 0)
+        {
+            glGetProgramInfoLog(program, 512, nullptr, infoLog);
+            std::cerr << "Error validating shader program\n" << infoLog << std::endl;
+            exit(1);
+        }
+    }
+
+public:
+    ~Shader()
+    {
+        glDeleteProgram(program);
+    }
+
+    Shader(const char* vsFile, const char* fsFile)
+    {
+        program = glCreateProgram();
+        if (program == 0)
+        {
+            fprintf(stderr, "Error creating shader program\n");
+            exit(1);
+        }
+
+        std::string vs, fs;
+
+        if (!Util::ReadFile(vsFile, vs)) exit(1);
+        AddShader(program, vs.c_str(), GL_VERTEX_SHADER);
+
+        if (!Util::ReadFile(fsFile, fs)) exit(1);
+        AddShader(program, fs.c_str(), GL_FRAGMENT_SHADER);
+
+        glLinkProgram(program);
+        CheckProgramLinking(program);
+    }
+
+    void SetVec3(const std::string& name, const glm::vec3& value) const
+    {
+        glUniform3fv(glGetUniformLocation(program, name.c_str()), 1, &value[0]);
+    }
+
+    void SetFloat(const std::string& name,const float& value) const
+    {
+        glUniform1f(glGetUniformLocation(program, name.c_str()), value);
+    }
+
+    [[nodiscard]] GLuint GetProgram() const { return program; }
+};
---
+++ 8-lab/task3/Shaders/shader.fs
@@ -0,0 +1,169 @@
+#version 330 core
+
+out vec4 FragColor;
+in vec2 TexCoords;
+
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 viewPos;
+
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+uniform vec3 lightAmbient; // цвет фона
+uniform float ambient;
+uniform float matAmbient;
+
+uniform float shininess;
+uniform vec3 specularColor;
+
+struct Hit {
+    float t;
+    vec3 norm;
+    bool hit;
+};
+
+Hit intersectParaboloid(vec3 ro, vec3 rd)
+{
+    Hit hit;
+    hit.hit = false;
+    hit.t = 1e20;
+    // z = x^2 + y^2
+    // P = Origin + t*D =>
+    // (Oz + t*Dz) = (Ox + t*Dx)^2 + (Oy + t*Dy)^2 =>
+    // At^2 + Bt + C = 0:
+    float A = rd.x * rd.x + rd.y * rd.y;
+    float B = 2.0 * (ro.x * rd.x + ro.y * rd.y) - rd.z;
+    float C = ro.x * ro.x + ro.y * ro.y - ro.z;
+
+    float det = B * B - 4.0 * A * C;
+
+    if (det < 0.0)
+    {
+        return hit;
+    }
+
+    float sqrtDet = sqrt(det);
+    float t1 = (-B - sqrtDet) / (2.0 * A);
+    float t2 = (-B + sqrtDet) / (2.0 * A);
+
+    // Проверка корней
+    for (int i = 0; i < 2; i++) {
+        float t = (i == 0) ? t1 : t2;
+        if (t <= 0.0) continue;
+        vec3 p = ro + t * rd;
+        // Ограничение по высоте: z от 0 до 1
+        if (p.z >= 0.0 && p.z <= 1.0) {
+            if (t < hit.t) {
+                hit.t = t;
+                // Нормаль: градиент f(x,y,z) = x^2 + y^2 - z -> (2x, 2y, -1)
+                hit.norm = normalize(vec3(2.0 * p.x, 2.0 * p.y, -1.0));
+                hit.hit = true;
+            }
+        }
+    }
+
+    // Тут будет крышка
+    // ..Lorem
+    return hit;
+}
+
+float random(vec3 seed) {
+    return fract(sin(dot(seed, vec3(12.9898, 78.233, 45.164))) * 43758.5453123);
+}
+
+float getShadowFactor(vec3 point, vec3 norm)
+{
+    float lightRadius = 0.1;
+    int samples = 50;
+    int hits = 0;
+
+    for (int i = 0; i < samples; i++) {
+        // небольшое случайное смещение для каждой итерации
+        vec3 offset = vec3(
+            random(point + float(i) * 0.1),
+            random(point + float(i) * 0.2),
+            random(point + float(i) * 0.3)
+        ) * 2.0 - 1.0;
+
+        // Направление луча со смещением
+        vec3 sampleLightPos = lightPos + offset * lightRadius;
+        vec3 lightDir = normalize(sampleLightPos - point);
+
+        // Сдвиг вдоль нормали(чтобы не врезаться в тот же объект)
+        vec3 shadowRayOrigin = point + norm * 0.001;
+
+        Hit hPara = intersectParaboloid(shadowRayOrigin,lightDir);
+
+        if (!hPara.hit) {
+            hits++;
+        }
+    }
+    // соотношение дошедших ко всем
+    return float(hits) / float(samples);
+}
+
+void main() {
+    // координаты пикселя (0..1) -> (-1..1)
+    vec4 pixCoords = vec4(TexCoords * 2.0 - 1.0, -1.0, 1.0);
+
+    // Инвертированная проекция и вид
+    mat4 invProj = inverse(projection);
+    mat4 invView = inverse(view);
+
+    // луч от глаза до пикселя на кваде
+    // invProj определяет тут степень расхождения лучей, как близко глаз к кваду
+    vec4 eyeCoords = invProj * pixCoords;
+    // Луч именно вперед z = -1 и он только поворачивается а не двигается w = 0
+    eyeCoords = vec4(eyeCoords.xy, -1.0, 0.0);
+
+    // перемножив на invView готовый луч исходит в другую сторону(поворот головы), но не перемещается
+    vec3 rayDir = normalize(vec3(invView * eyeCoords));
+    // позиция выпуска луча
+    vec3 rayOrigin = viewPos;
+
+    // Параболоид
+    Hit sceneHit = intersectParaboloid(rayOrigin, rayDir);
+
+    // Near <= 0.0 значит куб за спиной
+    if (sceneHit.hit) {
+
+        vec3 hitPos = rayOrigin + sceneHit.t * rayDir;
+
+        vec3 norm = sceneHit.norm;
+        if (dot(rayDir, norm) > 0.0) {
+            norm = -norm;
+        }
+
+        // Ambient
+        vec3 ambientResult = lightAmbient * (ambient * matAmbient) * objectColor;
+
+        vec3 lightDir = normalize(lightPos - hitPos);
+
+        vec3 diffuseResult = vec3(0.0);
+        vec3 specularResult = vec3(0.0);
+        // Тень
+        float shadowFactor = getShadowFactor(hitPos, norm);
+
+        // Диффузное освещение
+        float diff = max(dot(norm, lightDir), 0.0);
+        diffuseResult = diff * lightColor * objectColor * shadowFactor;
+
+        // Specular
+        // направление в камеру от объекта
+        vec3 viewDir = normalize(rayOrigin - hitPos);
+        // Blinn-Phong, вектор между lightDir и viewDir
+        vec3 halfwayDir = normalize(lightDir + viewDir);
+        // значение spec тем ближе к 1 чем меньше угол между norm и halfwayDir
+        float spec = pow(max(dot(norm, halfwayDir), 0.0), shininess);
+
+        specularResult = spec * specularColor * shadowFactor;
+
+        vec3 result = ambientResult + diffuseResult + specularResult;
+        FragColor = vec4(result, 1.0);
+    } else {
+        // Фон
+        FragColor = vec4(0.2, 0.2, 0.25, 1.0);
+    }
+}
